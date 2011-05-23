c6c54f954e035799a52bae4b5d42180b09a64f7d
Benjamin Eberlei <kontakt@beberlei.de>
2011-05-23 23:49:30 +0200

    Refactor Annotations to remove unnecessary caching layer, add FileCacheReader and modified CachedReader not to have a local map of annotations anymore

--- CachedReaderTest.php
+++ CachedReaderTest.php
@@ -3,13 +3,16 @@
 namespace Doctrine\Tests\Common\Annotations;
 
 use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\Cache\InMemoryCache;
 use Doctrine\Common\Annotations\CachedReader;
+use Doctrine\Common\Cache\ArrayCache;
 
 class CachedReaderTest extends AbstractReaderTest
 {
+    private $cache;
+    
     protected function getReader()
     {
-        return new CachedReader(new AnnotationReader(), new InMemoryCache());
+        $this->cache = new ArrayCache();
+        return new CachedReader(new AnnotationReader(), $this->cache);
     }
 }
