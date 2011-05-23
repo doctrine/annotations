2cc010c617753370381c28cef880a1b0987bc9b8
Benjamin Eberlei <kontakt@beberlei.de>
2011-05-23 22:14:28 +0200

    Fix some bugs in implementation

--- Regression/BCAnnotationReaderTest.php
+++ Regression/BCAnnotationReaderTest.php
@@ -2,9 +2,6 @@
 
 namespace Doctrine\Tests\Common\Annotations\Regression;
 
-use Doctrine\Tests\Common\Annotations\DummyAnnotation;
-use Doctrine\Tests\Common\Annotations\DummyJoinColumn;
-use Doctrine\Tests\Common\Annotations\Name;
 use ReflectionClass;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\IndexedReader;
@@ -25,24 +22,24 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $reader->setAutoloadAnnotations(false);
         $this->assertFalse($reader->getAutoloadAnnotations());
     
-        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\DummyClass');
+        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\Regression\DummyClass');
         $classAnnots = $reader->getClassAnnotations($class);
         
         $annotName = 'Doctrine\Tests\Common\Annotations\DummyAnnotation';
         $this->assertEquals(1, count($classAnnots));
-        $this->assertTrue($classAnnots[$annotName] instanceof DummyAnnotation);
+        $this->assertTrue($classAnnots[$annotName] instanceof \Doctrine\Tests\Common\Annotations\DummyAnnotation); // no use!
         $this->assertEquals("hello", $classAnnots[$annotName]->dummyValue);
         
         $field1Prop = $class->getProperty('field1');
         $propAnnots = $reader->getPropertyAnnotations($field1Prop);
         $this->assertEquals(1, count($propAnnots));
-        $this->assertTrue($propAnnots[$annotName] instanceof DummyAnnotation);
+        $this->assertTrue($propAnnots[$annotName] instanceof \Doctrine\Tests\Common\Annotations\DummyAnnotation); // no use!
         $this->assertEquals("fieldHello", $propAnnots[$annotName]->dummyValue);
         
         $getField1Method = $class->getMethod('getField1');
         $methodAnnots = $reader->getMethodAnnotations($getField1Method);
         $this->assertEquals(1, count($methodAnnots));
-        $this->assertTrue($methodAnnots[$annotName] instanceof DummyAnnotation);
+        $this->assertTrue($methodAnnots[$annotName] instanceof \Doctrine\Tests\Common\Annotations\DummyAnnotation); // no use!
         $this->assertEquals(array(1, 2, "three"), $methodAnnots[$annotName]->value);
         
         $field2Prop = $class->getProperty('field2');
@@ -52,8 +49,8 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $joinTableAnnot = $propAnnots['Doctrine\Tests\Common\Annotations\DummyJoinTable'];
         $this->assertEquals(1, count($joinTableAnnot->joinColumns));
         $this->assertEquals(1, count($joinTableAnnot->inverseJoinColumns));
-        $this->assertTrue($joinTableAnnot->joinColumns[0] instanceof DummyJoinColumn);
-        $this->assertTrue($joinTableAnnot->inverseJoinColumns[0] instanceof DummyJoinColumn);
+        $this->assertTrue($joinTableAnnot->joinColumns[0] instanceof \Doctrine\Tests\Common\Annotations\DummyJoinColumn); // no use!
+        $this->assertTrue($joinTableAnnot->inverseJoinColumns[0] instanceof \Doctrine\Tests\Common\Annotations\DummyJoinColumn); // no use!
         $this->assertEquals('col1', $joinTableAnnot->joinColumns[0]->name);
         $this->assertEquals('col2', $joinTableAnnot->joinColumns[0]->referencedColumnName);
         $this->assertEquals('col3', $joinTableAnnot->inverseJoinColumns[0]->name);
@@ -70,54 +67,13 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $this->assertEquals('hello', $classAnnot->dummyValue);
     }
 
-    public function testClassSyntaxErrorContext()
-    {
-        $this->setExpectedException(
-            "Doctrine\Common\Annotations\AnnotationException",
-            "[Syntax Error] Expected namespace separator or identifier, got ')' at position 18 in class Doctrine\Tests\Common\Annotations\DummyClassSyntaxError."
-        );
-
-        $class = new ReflectionClass('\Doctrine\Tests\Common\Annotations\DummyClassSyntaxError');
-
-        $reader = $this->createAnnotationReader();
-        $reader->getClassAnnotations($class);
-    }
-
-    public function testMethodSyntaxErrorContext()
-    {
-        $this->setExpectedException(
-            "Doctrine\Common\Annotations\AnnotationException",
-            "[Syntax Error] Expected namespace separator or identifier, got ')' at position 18 in method Doctrine\Tests\Common\Annotations\DummyClassMethodSyntaxError::foo()."
-        );
-
-        $class = new ReflectionClass('\Doctrine\Tests\Common\Annotations\DummyClassMethodSyntaxError');
-        $method = $class->getMethod('foo');
-
-        $reader = $this->createAnnotationReader();
-        $reader->getMethodAnnotations($method);
-    }
-
-    public function testPropertySyntaxErrorContext()
-    {
-        $this->setExpectedException(
-            "Doctrine\Common\Annotations\AnnotationException",
-            "[Syntax Error] Expected namespace separator or identifier, got ')' at position 18 in property Doctrine\Tests\Common\Annotations\DummyClassPropertySyntaxError::\$foo."
-        );
-
-        $class = new ReflectionClass('\Doctrine\Tests\Common\Annotations\DummyClassPropertySyntaxError');
-        $property = $class->getProperty('foo');
-
-        $reader = $this->createAnnotationReader();
-        $reader->getPropertyAnnotations($property);
-    }
-
     /**
      * @group regression
      */
     public function testMultipleAnnotationsOnSameLine()
     {
         $reader = $this->createAnnotationReader();
-        $class = new ReflectionClass('\Doctrine\Tests\Common\Annotations\DummyClass2');
+        $class = new ReflectionClass('\Doctrine\Tests\Common\Annotations\Regression\DummyClass2');
         $annotations = $reader->getPropertyAnnotations($class->getProperty('id'));
         $this->assertEquals(3, count($annotations));
     }
@@ -133,7 +89,7 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
             }
         });
 
-        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\DummyClass');
+        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\Regression\DummyClass');
         $classAnnots = $reader->getClassAnnotations($class);
         $this->assertTrue(isset($classAnnots['Doctrine\Tests\Common\Annotations\Regression\CustomDummyAnnotationClass']));
         $annot = $classAnnots['Doctrine\Tests\Common\Annotations\Regression\CustomDummyAnnotationClass'];
@@ -144,7 +100,7 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
     {
         $reader = $this->createAnnotationReader();
 
-        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\DummyClassNonAnnotationProblem');
+        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\Regression\DummyClassNonAnnotationProblem');
         $annotations = $reader->getPropertyAnnotations($class->getProperty('foo'));
         $this->assertArrayHasKey('Doctrine\Tests\Common\Annotations\DummyAnnotation', $annotations);
         $this->assertInstanceOf('Doctrine\Tests\Common\Annotations\DummyAnnotation', $annotations['Doctrine\Tests\Common\Annotations\DummyAnnotation']);
@@ -176,7 +132,7 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $reader = new AnnotationReader(new \Doctrine\Common\Cache\ArrayCache);
         $reader->setDefaultAnnotationNamespace('Doctrine\Tests\Common\Annotations\\');
         
-        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\DummyClassWithEmail');
+        $class = new ReflectionClass('Doctrine\Tests\Common\Annotations\Regression\DummyClassWithEmail');
         $classAnnots = $reader->getClassAnnotations($class);
         
         $this->assertEquals(1, count($classAnnots));
@@ -192,7 +148,7 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $result = $reader->getPropertyAnnotations($reflClass->getProperty('bar'));
         $this->assertEquals(1, count($result));
         $annot = $result['Doctrine\Tests\Common\Annotations\Name'];
-        $this->assertTrue($annot instanceof Name);
+        $this->assertTrue($annot instanceof \Doctrine\Tests\Common\Annotations\Name); // no use!
         $this->assertEquals('bar', $annot->foo);
     }
 
@@ -209,7 +165,7 @@ class BCAnnotationReaderTest extends \Doctrine\Tests\DoctrineTestCase
         $result = $reader->getPropertyAnnotations($reflClass->getProperty('foo'));
         $this->assertEquals(1, count($result));
         $annot = $result['Doctrine\Tests\Common\Annotations\Name'];
-        $this->assertTrue($annot instanceof Name);
+        $this->assertTrue($annot instanceof \Doctrine\Tests\Common\Annotations\Name); // no use!
         $this->assertEquals('bar', $annot->foo);
     }
 }
@@ -237,4 +193,79 @@ class AliasNamespace
      * @alias:Annotations\Name(foo="bar")
      */
     public $foo;
+}
+
+/**
+ * A description of this class.
+ *
+ * Let's see if the parser recognizes that this @ is not really referring to an
+ * annotation. Also make sure that @var \ is not concated to "@var\is".
+ * 
+ * Copy of the class one namespace up to avoid matches against __NAMESPACE__
+ *
+ * @author robo
+ * @since 2.0
+ * @DummyAnnotation(dummyValue="hello")
+ */
+class DummyClass {
+    /**
+     * A nice property.
+     *
+     * @var mixed
+     * @DummyAnnotation(dummyValue="fieldHello")
+     */
+    private $field1;
+
+    /**
+     * @DummyJoinTable(name="join_table",
+     *      joinColumns={@DummyJoinColumn(name="col1", referencedColumnName="col2")},
+     *      inverseJoinColumns={
+     *          @DummyJoinColumn(name="col3", referencedColumnName="col4")
+     *      })
+     */
+    private $field2;
+
+    /**
+     * Gets the value of field1.
+     *
+     * @return mixed
+     * @DummyAnnotation({1,2,"three"})
+     */
+    public function getField1() {
+    }
+}
+
+
+/**
+ * @ignoreAnnotation("var")
+ */
+class DummyClass2 {
+    /**
+     * @DummyId @DummyColumn(type="integer") @DummyGeneratedValue
+     * @var integer
+     */
+    private $id;
+}
+
+/**
+ * @ignoreAnnotation({"since", "var"})
+ */
+class DummyClassNonAnnotationProblem
+{
+    /**
+     * @DummyAnnotation
+     *
+     * @var \Test
+     * @since 0.1
+     */
+    public $foo;
+}
+
+
+/**
+* @DummyAnnotation Foo bar <foobar@1domain.com>
+*/
+class DummyClassWithEmail
+{
+    
 }
