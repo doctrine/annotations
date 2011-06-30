915a0873822605ff0ee0644d1e98d56e6f2a7230
Benjamin Eberlei <kontakt@beberlei.de>
2011-06-30 10:39:29 +0200

    Remove last concept of \\Annotation matching and introduce check for either Doctrine\Common\Annotations\Annotation or for @Annotation in the class level docblock

--- Ticket/DCOM55Test.php
+++ Ticket/DCOM55Test.php
@@ -17,12 +17,47 @@ class DCOM55Test extends \PHPUnit_Framework_TestCase
         
         $this->assertEquals(0, count($annots));
     }
+    
+    public function testAnnotation()
+    {
+        $class = new \ReflectionClass(__NAMESPACE__ . '\\DCOM55Consumer');
+        $reader = new \Doctrine\Common\Annotations\AnnotationReader();
+        $annots = $reader->getClassAnnotations($class);
+        
+        $this->assertEquals(1, count($annots));
+        $this->assertInstanceOf(__NAMESPACE__.'\\DCOM55Annotation', $annots[0]);
+    }
+    
+    public function testParseAnnotationDocblocks()
+    {
+        $class = new \ReflectionClass(__NAMESPACE__ . '\\DCOM55Annotation');
+        $reader = new \Doctrine\Common\Annotations\AnnotationReader();
+        $annots = $reader->getClassAnnotations($class);
+        
+        $this->assertEquals(0, count($annots));
+    }
 }
 
 /**
  * @Controller
  */
 class Dummy
+{
+    
+}
+
+/**
+ * @Annotation
+ */
+class DCOM55Annotation
+{
+    
+}
+
+/**
+ * @DCOM55Annotation
+ */
+class DCOM55Consumer
 {
     
 }
