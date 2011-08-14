0cda2e0acbf862f7724ad77449ab4d68a2106bf7
Johannes Schmitt <schmittjoh@gmail.com>
2011-08-14 15:52:27 +0200

    re-arranged methods, changed test case slightly

--- SimpleAnnotationReaderTest.php
+++ SimpleAnnotationReaderTest.php
@@ -6,32 +6,22 @@ use Doctrine\Common\Annotations\SimpleAnnotationReader;
 
 class SimpleAnnotationReaderTest extends AbstractReaderTest
 {
-
-    protected function getReader()
-    {
-        $reader = new SimpleAnnotationReader();
-        $reader->addNamespace(__NAMESPACE__);
-        $reader->addNamespace(__NAMESPACE__ . '\Fixtures');
-
-        return $reader;
-    }
-
     /**
-     * Expected Exception invalid for SimpleAnnotationReader
+     * Contrary to the behavior of the default annotation reader, we do just ignore
+     * these in the simple annotation reader (so, no expected exception here).
      */
     public function testImportDetectsNotImportedAnnotation()
     {
         parent::testImportDetectsNotImportedAnnotation();
-        $this->assertTrue(true);
     }
 
     /**
-     * Expected Exception invalid for SimpleAnnotationReader
+     * Contrary to the behavior of the default annotation reader, we do just ignore
+     * these in the simple annotation reader (so, no expected exception here).
      */
     public function testImportDetectsNonExistentAnnotation()
     {
         parent::testImportDetectsNonExistentAnnotation();
-        $this->assertTrue(true);
     }
 
     /**
@@ -42,4 +32,12 @@ class SimpleAnnotationReaderTest extends AbstractReaderTest
         parent::testInvalidAnnotationUsageButIgnoredClass();
     }
 
+    protected function getReader()
+    {
+        $reader = new SimpleAnnotationReader();
+        $reader->addNamespace(__NAMESPACE__);
+        $reader->addNamespace(__NAMESPACE__ . '\Fixtures');
+
+        return $reader;
+    }
 }
