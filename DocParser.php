776ac31fa23964977aabe49d95749b75597d03f2
Fabio B. Silva <fabio.bat.silva@gmail.com>
2012-02-20 13:35:53 -0200

    ignore target if is not valid and ignores is true

--- DocParser.php
+++ DocParser.php
@@ -555,7 +555,7 @@ final class DocParser
         // only process names which are not fully qualified, yet
         // fully qualified names must start with a \
         $originalName = $name;
-        if ('\\' !== $name[0] && $name[0] === strtoupper($name[0])) {
+        if ('\\' !== $name[0]) {
             $alias = (false === $pos = strpos($name, '\\'))? $name : substr($name, 0, $pos);
 
             $found = false;
@@ -591,9 +591,6 @@ final class DocParser
         }
 
         if (!$this->classExists($name)) {
-            if ($this->ignoreNotImportedAnnotations || isset($this->ignoredAnnotationNames[$name])) {
-                return false;
-            }
             throw AnnotationException::semanticalError(sprintf('The annotation "@%s" in %s does not exist, or could not be auto-loaded.', $name, $this->context));
         }
 
@@ -624,6 +621,10 @@ final class DocParser
 
         //if annotation does not support current target
         if (0 === (self::$annotationMetadata[$name]['targets'] & $target) && $target) {
+            if ($this->ignoreNotImportedAnnotations || isset($this->ignoredAnnotationNames[$originalName])) {
+                return false;
+            }
+
             throw AnnotationException::semanticalError(
                 sprintf('Annotation @%s is not allowed to be declared on %s. You may only use this annotation on these code elements: %s.',
                      $originalName, $this->context, self::$annotationMetadata[$name]['targets_literal'])
