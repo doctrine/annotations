d3e88db068fb975c9d5215dc72386220a7115e10
Victor Guedes <victorguedes@gmail.com>
2013-01-03 10:39:44 -0200

    Update tests/Doctrine/Tests/Common/Annotations/Fixtures/NamespaceWithClosureDeclaration.php
    Statement duplicate
    
    $var = 1;
    function () use ($var) {};

--- Fixtures/NamespaceWithClosureDeclaration.php
+++ Fixtures/NamespaceWithClosureDeclaration.php
@@ -2,9 +2,6 @@
 
 namespace Doctrine\Tests\Common\Annotations\Fixtures;
 
-$var = 1;
-function () use ($var) {};
-
 use Doctrine\Tests\Common\Annotations\Fixtures\Annotation\Secure;
 use Doctrine\Tests\Common\Annotations\Fixtures\Annotation\Route;
 use Doctrine\Tests\Common\Annotations\Fixtures\Annotation\Template;
