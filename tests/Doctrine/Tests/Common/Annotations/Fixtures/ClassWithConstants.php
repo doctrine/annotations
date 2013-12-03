90eb2b7c88a05e10f4a52cb99b77706d815792d2
Marco Pivetta <ocramius@gmail.com>
2013-12-03 16:27:58 +0100

    Adding failing test for DCOM-179
    Constants ending with `_` are not considered by the parser

--- tests/Doctrine/Tests/Common/Annotations/Fixtures/ClassWithConstants.php
+++ tests/Doctrine/Tests/Common/Annotations/Fixtures/ClassWithConstants.php
@@ -4,7 +4,7 @@ namespace Doctrine\Tests\Common\Annotations\Fixtures;
 
 class ClassWithConstants
 {
-
     const SOME_VALUE = 'ClassWithConstants.SOME_VALUE';
     const SOME_KEY   = 'ClassWithConstants.SOME_KEY';
+    const OTHER_KEY_ = 'ClassWithConstants.OTHER_KEY_';
 }
