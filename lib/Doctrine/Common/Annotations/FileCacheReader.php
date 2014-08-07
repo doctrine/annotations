b14972145efb6f097e84f78fccd8ac6ec1987bc4
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2014-08-07 11:57:16 +0200

    fix saveCacheFile...

--- lib/Doctrine/Common/Annotations/FileCacheReader.php
+++ lib/Doctrine/Common/Annotations/FileCacheReader.php
@@ -64,10 +64,6 @@ class FileCacheReader implements Reader
     public function __construct(Reader $reader, $cacheDir, $debug = false)
     {
         $this->reader = $reader;
-        if (!is_dir($cacheDir) && !@mkdir($cacheDir, 0777, true)) {
-            throw new \InvalidArgumentException(sprintf('The directory "%s" does not exist and could not be created.', $cacheDir));
-        }
-
         $this->dir   = rtrim($cacheDir, '\\/');
         $this->debug = $debug;
     }
@@ -186,7 +182,10 @@ class FileCacheReader implements Reader
      */
     private function saveCacheFile($path, $data)
     {
-        if (!is_writable($this->dir)) {
+        if (!is_dir($this->dir) && !@mkdir($this->dir, 0777, true)) {
+            throw new \InvalidArgumentException(sprintf('The directory "%s" does not exist and could not be created.', $this->dir));
+        }
+        if (!is_writable($this->dir . '/')) {
             throw new \InvalidArgumentException(sprintf('The directory "%s" is not writable. Both, the webserver and the console user need access. You can manage access rights for multiple users with "chmod +a". If your system does not support this, check out the acl package.', $this->dir));
         }
         file_put_contents($path, '<?php return unserialize('.var_export(serialize($data), true).');');
