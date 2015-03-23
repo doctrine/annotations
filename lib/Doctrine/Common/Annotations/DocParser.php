366b7fe8c262737fa2b89371031b19e748c364eb
Masoud Zohrabi <mdzzohrabi@gmail.com>
2015-03-23 21:04:32 +0430

    Add $context property to annotation constructor
    For some purpose we need to detect annotation target, class or property name.
    for sample , in a Column annotation that has a tableName property i want to create this property if it not set by user from class property name.
    class Blog {
           /** @Column **/
           protected $Author_Id;
    }
    
    /**
     * @Annotation
     * @Target({ "PROPERTY" })
     */
    class Column {
          public $tableName;
          public __construct( array $values , $context ) {
                 $propertyName = substr( $context , strpos( $context , '$' ) + 1 );
                 $this->tableName = String::underscore( $propertyName );
          } 
    }

--- lib/Doctrine/Common/Annotations/DocParser.php
+++ lib/Doctrine/Common/Annotations/DocParser.php
@@ -791,7 +791,7 @@ final class DocParser
         // check if the annotation expects values via the constructor,
         // or directly injected into public properties
         if (self::$annotationMetadata[$name]['has_constructor'] === true) {
-            return new $name($values);
+            return new $name($values, $this->context);
         }
 
         $instance = new $name();
