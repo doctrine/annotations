10a0a9d9e7f6231d111735b108770d81c76e4242
mpalourdio <mpalourdio@gmail.com>
2015-06-16 15:52:32 +0200

    Use PHP_VERSION_ID, and not PHP_VERSION

--- lib/Doctrine/Common/Annotations/AnnotationReader.php
+++ lib/Doctrine/Common/Annotations/AnnotationReader.php
@@ -162,7 +162,7 @@ class AnnotationReader implements Reader
             throw AnnotationException::optimizerPlusSaveComments();
         }
 
-        if (PHP_VERSION < 70000) {
+        if (PHP_VERSION_ID < 70000) {
             if (extension_loaded('Zend Optimizer+') && (ini_get('zend_optimizerplus.load_comments') === "0" || ini_get('opcache.load_comments') === "0")) {
                 throw AnnotationException::optimizerPlusLoadComments();
             }
