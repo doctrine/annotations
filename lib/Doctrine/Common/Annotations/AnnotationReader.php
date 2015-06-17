6eeadf5fd5d714065a59d5da8cbcd24196da667a
Benjamin Eberlei <kontakt@beberlei.de>
2015-06-17 14:13:27 +0200

    Fix broken merge on Zend Opcache check.

--- lib/Doctrine/Common/Annotations/AnnotationReader.php
+++ lib/Doctrine/Common/Annotations/AnnotationReader.php
@@ -154,20 +154,12 @@ class AnnotationReader implements Reader
      */
     public function __construct()
     {
-        if (extension_loaded('Zend Optimizer+') && (ini_get('zend_optimizerplus.save_comments') === "0" || ini_get('opcache.save_comments') === "0")) {
-            throw AnnotationException::optimizerPlusSaveComments();
-        }
-
-        if (extension_loaded('Zend OPcache') && ini_get('opcache.save_comments') == 0) {
-            throw AnnotationException::optimizerPlusSaveComments();
-        }
-
         if (PHP_VERSION_ID < 70000) {
             if (extension_loaded('Zend Optimizer+') && (ini_get('zend_optimizerplus.load_comments') === "0" || ini_get('opcache.load_comments') === "0")) {
                 throw AnnotationException::optimizerPlusLoadComments();
             }
 
-            if (extension_loaded('Zend OPcache') && ini_get('opcache.load_comments') == 0) {
+            if (extension_loaded('Zend OPcache') && ini_get('opcache.load_comments') === "0") {
                 throw AnnotationException::optimizerPlusLoadComments();
             }
         }
