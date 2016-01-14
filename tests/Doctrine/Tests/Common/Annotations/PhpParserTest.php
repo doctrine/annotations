eeabea9cefcfba7dc9b66d2fe3f91a2c7e28b74e
Paul Oldridge <me@pauloldridge.com>
2016-01-13 16:22:28 -0800

    Added test for PHP7 group use statements

--- tests/Doctrine/Tests/Common/Annotations/PhpParserTest.php
+++ tests/Doctrine/Tests/Common/Annotations/PhpParserTest.php
@@ -33,6 +33,18 @@ class PhpParserTest extends \PHPUnit_Framework_TestCase
         ), $parser->parseClass($class));
     }
 
+    public function testParseClassWithGroupUseStatement()
+    {
+        $class = new ReflectionClass(__NAMESPACE__ . '\Fixtures\GroupUseStatement');
+        $parser = new PhpParser();
+
+        $this->assertEquals(array(
+            'route'  => __NAMESPACE__ . '\Fixtures\Annotation\Route',
+            'supersecure' => __NAMESPACE__ . '\Fixtures\Annotation\Secure',
+            'template' => __NAMESPACE__ . '\Fixtures\Annotation\Template',
+        ), $parser->parseClass($class));
+    }
+
     public function testParseClassWhenNotUserDefined()
     {
         $parser = new PhpParser();
