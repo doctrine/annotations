18ec721da1127b5abdb6627622016216758ac9ca
Marco Pivetta <ocramius@gmail.com>
2016-12-30 15:00:50 +0100

    Removing unsafe `uniqid()` usage

--- tests/Doctrine/Tests/Common/Annotations/FileCacheReaderTest.php
+++ tests/Doctrine/Tests/Common/Annotations/FileCacheReaderTest.php
@@ -11,7 +11,7 @@ class FileCacheReaderTest extends AbstractReaderTest
 
     protected function getReader()
     {
-        $this->cacheDir = sys_get_temp_dir() . '/annotations_' . uniqid();
+        $this->cacheDir = sys_get_temp_dir() . '/annotations_' . uniqid('', true);
         @mkdir($this->cacheDir);
         return new FileCacheReader(new AnnotationReader(), $this->cacheDir);
     }
@@ -29,7 +29,7 @@ class FileCacheReaderTest extends AbstractReaderTest
      */
     public function testAttemptToCreateAnnotationCacheDir()
     {
-        $this->cacheDir = sys_get_temp_dir() . '/not_existed_dir_' . uniqid();
+        $this->cacheDir = sys_get_temp_dir() . '/not_existed_dir_' . uniqid('', true);
 
         $this->assertFalse(is_dir($this->cacheDir));
 
