f959393d36734b2eeb2528996bc706cb8701b403
Adam Žurek <thejabko@gmail.com>
2019-03-12 00:01:29 +0100

    phpstan updated to 0.11.3

--- lib/Doctrine/Annotations/Annotation/Attributes.php
+++ lib/Doctrine/Annotations/Annotation/Attributes.php
@@ -2,6 +2,8 @@
 
 namespace Doctrine\Annotations\Annotation;
 
+use Doctrine;
+
 /**
  * Annotation that can be used to signal to the parser
  * to check the types of all declared attributes during the parsing process.
--- lib/Doctrine/Annotations/Annotation/Target.php
+++ lib/Doctrine/Annotations/Annotation/Target.php
@@ -44,7 +44,7 @@ final class Target
     /**
      * Literal target declaration.
      *
-     * @var integer
+     * @var string
      */
     public $literal;
 
