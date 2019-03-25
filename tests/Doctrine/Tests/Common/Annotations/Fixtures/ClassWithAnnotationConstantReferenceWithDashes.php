ee69aaf50967168ddc5a28c6afaff9b8d4c042f7
Marco Pivetta <ocramius@gmail.com>
2019-03-25 18:18:20 +0100

    Ensuring that `-` in identifiers won't happen in annotation reading

---
+++ tests/Doctrine/Tests/Common/Annotations/Fixtures/ClassWithAnnotationConstantReferenceWithDashes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Doctrine\Tests\Common\Annotations\Fixtures;
+
+use Doctrine\Tests\Common\Annotations\Fixtures\Annotation\Route;
+
+/** @Route(@AValid-constant::REFERENCE) */
+class ClassWithAnnotationConstantReferenceWithDashes
+{
+}
