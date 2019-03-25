818f160a3e159f245b9995ef02fa32f1ef772e07
Marco Pivetta <ocramius@gmail.com>
2019-03-25 18:18:20 +0100

    Ensuring that `-` in identifiers won't happen in annotation reading

---
+++ tests/Doctrine/Tests/Common/Annotations/Fixtures/ClassWithInvalidAnnotationContainingDashes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Doctrine\Tests\Common\Annotations\Fixtures;
+
+/**
+ * @please-do-not-parse-me
+ * @AlsoDoNot\Parse-me
+ */
+class ClassWithInvalidAnnotationContainingDashes
+{
+}
