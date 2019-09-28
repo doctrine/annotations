36099553bbb7e5356b46e14bb7811edee10b6ea4
davidbyoung <dbyoung2@gmail.com>
2019-09-28 08:30:31 -0500

    Fixed DocParser lookahead to work in PHP 7.4
    Fixed spl autoload function to return void per PHP documentation
    
    Removed unnecessary ignored error message from phpstan config that is no longer thrown because of the fixes to DocParser

--- lib/Doctrine/Annotations/DocParser.php
+++ lib/Doctrine/Annotations/DocParser.php
@@ -980,8 +980,11 @@ final class DocParser
 
         $className = $this->lexer->token['value'];
 
-        while ($this->lexer->lookahead['position'] === ($this->lexer->token['position'] + strlen($this->lexer->token['value']))
-                && $this->lexer->isNextToken(DocLexer::T_NAMESPACE_SEPARATOR)) {
+        while (
+            null !== $this->lexer->lookahead &&
+            $this->lexer->lookahead['position'] === ($this->lexer->token['position'] + strlen($this->lexer->token['value'])) &&
+            $this->lexer->isNextToken(DocLexer::T_NAMESPACE_SEPARATOR)
+        ) {
 
             $this->match(DocLexer::T_NAMESPACE_SEPARATOR);
             $this->matchAny(self::$classIdentifiers);
